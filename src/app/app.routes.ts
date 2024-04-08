487c9657178414f1b1f4542dc65a9fe2d1c75fd6
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-04-08 17:55:44 +0530

    ui components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCategoryComponent } from './pages/single-category/single-category.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { TermsConditionsComponent } from './pages/terms-conditions/terms-conditions.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'category', component: SingleCategoryComponent},
+    {path: 'post', component: SinglePostComponent},
+    {path: 'about', component: TermsConditionsComponent},
+    {path: 'contact', component: ContactComponent}
+];
