1b7a39900eda0a6f12fd10d4de62af5d5f602d70
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-04-08 21:54:57 +0530

    finishing all ui except contact us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SingleCategoryComponent } from './pages/single-category/single-category
 import { SinglePostComponent } from './pages/single-post/single-post.component';
 import { TermsConditionsComponent } from './pages/terms-conditions/terms-conditions.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'category', component: SingleCategoryComponent},
     {path: 'post', component: SinglePostComponent},
-    {path: 'about', component: TermsConditionsComponent},
-    {path: 'contact', component: ContactComponent}
+    {path: 'about', component: AboutComponent},
+    {path: 'contact', component: ContactComponent},
+    {path: 'termsandconditions', component: TermsConditionsComponent}
 ];
