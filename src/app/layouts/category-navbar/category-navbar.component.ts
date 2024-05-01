833b4f14185229e3ffe35e40fbf7218d19a5f48b
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-05-01 19:25:45 +0530

    setting up firebase and making category-nav dynamic

--- src/app/layouts/category-navbar/category-navbar.component.ts
+++ src/app/layouts/category-navbar/category-navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { CategoriesService } from '../../services/categories.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-category-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './category-navbar.component.html',
   styleUrl: './category-navbar.component.css'
 })
 export class CategoryNavbarComponent {
+  categoryArray : Array<any> = [];
 
+  constructor(private categoryService: CategoriesService){}
+
+  ngOnInit(): void{
+    this.categoryService.loadData().subscribe( val => this.categoryArray = val);
+  }
 }
