56aff723ea41e56b7c3159716429d08a7d7bf5f4
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-05-02 21:56:04 +0530

    featured posts feature

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
+import { PostService } from '../../services/post.service';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,8 @@ import { PostCardComponent } from '../../layouts/post-card/post-card.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private postService : PostService){
+    this.postService.loadData().subscribe(val => console.log(val))
+  }
 
 }
