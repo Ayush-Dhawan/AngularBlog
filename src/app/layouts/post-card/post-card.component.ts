c85c34475d1a7ff636c825dc540bf8c78bd5d151
Ayush-Dhawan <ayushdhawan102004@gmail.com>
2024-05-02 23:52:48 +0530

    making featured posts dynamic

--- src/app/layouts/post-card/post-card.component.ts
+++ src/app/layouts/post-card/post-card.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './post-card.component.html',
   styleUrl: './post-card.component.css'
 })
 export class PostCardComponent {
 
+  @Input() postData!: any;
+  
+  ngOnInit(): void{
+    console.log("post data: ",this.postData)
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { PostCardComponent } from '../../layouts/post-card/post-card.component';
 import { PostService } from '../../services/post.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PostCardComponent],
+  imports: [PostCardComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  featuredPosts !: Array<Object>;
   constructor(private postService : PostService){
-    this.postService.loadData().subscribe(val => console.log(val))
+    this.postService.loadData().subscribe(val => this.featuredPosts = val)
   }
 
 }
